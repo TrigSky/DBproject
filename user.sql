a4aff72a5ec4b3fd856d9fa6e0dd9aef3ee7d307
Demarcia <583181285@qq.com>
2015-12-01 18:49:27 +0800

    user.sql

---
+++ user.sql
@@ -0,0 +1,33 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : Mysql
+Source Server Version : 50623
+Source Host           : localhost:3306
+Source Database       : alpha
+
+Target Server Type    : MYSQL
+Target Server Version : 50623
+File Encoding         : 65001
+
+Date: 2015-12-01 18:48:39
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `user_id` varchar(255) NOT NULL DEFAULT '123456',
+  `user_name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1234', 'foo', 'foo');
+INSERT INTO `user` VALUES ('testid', 'testname', 'testpass');
