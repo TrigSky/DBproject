27b2f269861c7a2894a677e7b8cde6c9fcebd318
Demarcia <583181285@qq.com>
2015-12-04 19:40:06 +0800

    domain investoption.java

---
+++ src/main/java/domain/InvestOption.java
@@ -0,0 +1,81 @@
+package main.java.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "invest_option")
+public class InvestOption
+{
+    @Id
+    private String uuid;
+    private double price;
+    private String title;
+    private String detail;
+    @Column(name = "imgs_arr")
+    private String imgsArr;
+    @Column(name = "peo_num")
+    private int peopleNum;
+
+    public String getUuid()
+    {
+        return uuid;
+    }
+
+    public void setUuid(String uuid)
+    {
+        this.uuid = uuid;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getDetail()
+    {
+        return detail;
+    }
+
+    public void setDetail(String detail)
+    {
+        this.detail = detail;
+    }
+
+    public String getImgsArr()
+    {
+        return imgsArr;
+    }
+
+    public void setImgsArr(String imgsArr)
+    {
+        this.imgsArr = imgsArr;
+    }
+
+    public int getPeopleNum()
+    {
+        return peopleNum;
+    }
+
+    public void setPeopleNum(int peopleNum)
+    {
+        this.peopleNum = peopleNum;
+    }
+}
